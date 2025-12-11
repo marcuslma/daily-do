ef17cbe8bcff2fcef53011721402205675f7056d
Marcus Maurmann <marcuslma@gmail.com>
2025-12-11 14:52:57 -0300

    Fix the scroll wheel on dialog over dialog

--- src/components/todo/todo-create-dialog.tsx
+++ src/components/todo/todo-create-dialog.tsx
@@ -261,9 +261,14 @@ export function TodoCreateDialog({
                       {emoji || <Smile className="size-4" />}
                     </Button>
                   </PopoverTrigger>
-                  <PopoverContent align="start" className="w-auto p-0">
+                  <PopoverContent
+                    align="start"
+                    className="w-auto p-0"
+                    onWheel={(e) => e.stopPropagation()}
+                  >
                     <EmojiPicker
                       emojiStyle={EmojiStyle.NATIVE}
+                      lazyLoadEmojis
                       onEmojiClick={handleEmojiSelect}
                       previewConfig={{ showPreview: false }}
                       searchPlaceHolder="Buscar emoji..."
